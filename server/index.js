2f0002462b6002da1253c0fedc9680bde883c3d3
Purujit-Kulshreshtha <68121424+Purujit-Kulshreshtha@users.noreply.github.com>
2021-10-04 05:22:12 +0000

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(cors())
 app.use('/users', userRouter);
 
 const PORT = process.env.PORT || 5000;
-const DATABASEURI = "mongodb+srv://purujit:iDreamPassword@idream.rxyrv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const DATABASEURI = //mongoDB Atlas URI Here
 
 mongoose.connect(DATABASEURI, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
 		app.listen(PORT, () => console.log(`Server running: ${PORT}`))
